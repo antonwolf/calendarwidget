d4916989d2715c494ffc07467d772ecdff0a6e23
mail@antonwolf.de <mail@antonwolf.de@324a29b3-b407-d322-dbe5-d6661dea56ef>
2011-03-04 07:45:19 +0000

    Adding copyright notices to the various widget providers
    git-svn-id: http://antons-apps.googlecode.com/svn/trunk@15 324a29b3-b407-d322-dbe5-d6661dea56ef

--- gen/de/antonwolf/agendawidget/R.java
+++ gen/de/antonwolf/agendawidget/R.java
@@ -66,12 +66,10 @@ public final class R {
         public static final int WidgetTitle=0x7f090001;
     }
     public static final class xml {
-        public static final int agenda_widget_2x1=0x7f040000;
-        public static final int agenda_widget_2x2=0x7f040001;
-        public static final int agenda_widget_3x1=0x7f040002;
-        public static final int agenda_widget_3x2=0x7f040003;
-        public static final int agenda_widget_3x3=0x7f040004;
-        public static final int agenda_widget_4x1=0x7f040005;
-        public static final int agenda_widget_4x2=0x7f040006;
+        public static final int agenda_widget_3x1=0x7f040000;
+        public static final int agenda_widget_3x2=0x7f040001;
+        public static final int agenda_widget_3x3=0x7f040002;
+        public static final int agenda_widget_4x1=0x7f040003;
+        public static final int agenda_widget_4x2=0x7f040004;
     }
 }
