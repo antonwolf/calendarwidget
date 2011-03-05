c98a69d0b8afd2288bf115fe03ec7782e52c6bf4
mail@antonwolf.de <mail@antonwolf.de@324a29b3-b407-d322-dbe5-d6661dea56ef>
2011-03-05 14:11:11 +0000

    Adding more widget sizes
    git-svn-id: http://antons-apps.googlecode.com/svn/trunk@30 324a29b3-b407-d322-dbe5-d6661dea56ef

--- gen/de/antonwolf/agendawidget/R.java
+++ gen/de/antonwolf/agendawidget/R.java
@@ -55,24 +55,25 @@ public final class R {
         public static final int format_this_year=0x7f050003;
         public static final int format_time=0x7f050005;
         public static final int format_today=0x7f050001;
-        public static final int label_2x1=0x7f050016;
-        public static final int label_2x2=0x7f050017;
-        public static final int label_3x1=0x7f050008;
-        public static final int label_3x2=0x7f050009;
-        public static final int label_3x3=0x7f05000a;
+        public static final int label_2x1=0x7f05000d;
+        public static final int label_3x1=0x7f05000a;
+        public static final int label_3x2=0x7f05000b;
+        public static final int label_3x3=0x7f05000c;
         public static final int label_4x1=0x7f050006;
         public static final int label_4x2=0x7f050007;
-        public static final int open_calendar=0x7f05000b;
-        public static final int settings_birthdays=0x7f05000c;
-        public static final int settings_birthdays_display=0x7f050010;
-        public static final int settings_birthdays_display_hide=0x7f050012;
-        public static final int settings_birthdays_display_show=0x7f050011;
-        public static final int settings_birthdays_recognition=0x7f05000d;
-        public static final int settings_birthdays_recognition_normal=0x7f05000f;
-        public static final int settings_birthdays_recognition_special=0x7f05000e;
-        public static final int settings_calendars=0x7f050013;
-        public static final int settings_calendars_hide=0x7f050015;
-        public static final int settings_calendars_show=0x7f050014;
+        public static final int label_4x3=0x7f050008;
+        public static final int label_4x4=0x7f050009;
+        public static final int open_calendar=0x7f05000e;
+        public static final int settings_birthdays=0x7f05000f;
+        public static final int settings_birthdays_display=0x7f050013;
+        public static final int settings_birthdays_display_hide=0x7f050015;
+        public static final int settings_birthdays_display_show=0x7f050014;
+        public static final int settings_birthdays_recognition=0x7f050010;
+        public static final int settings_birthdays_recognition_normal=0x7f050012;
+        public static final int settings_birthdays_recognition_special=0x7f050011;
+        public static final int settings_calendars=0x7f050016;
+        public static final int settings_calendars_hide=0x7f050018;
+        public static final int settings_calendars_show=0x7f050017;
     }
     public static final class style {
         public static final int PickAction=0x7f090005;
@@ -83,10 +84,13 @@ public final class R {
         public static final int WidgetTitle=0x7f090001;
     }
     public static final class xml {
-        public static final int agenda_widget_3x1=0x7f040000;
-        public static final int agenda_widget_3x2=0x7f040001;
-        public static final int agenda_widget_3x3=0x7f040002;
-        public static final int agenda_widget_4x1=0x7f040003;
-        public static final int agenda_widget_4x2=0x7f040004;
+        public static final int agenda_widget_2x1=0x7f040000;
+        public static final int agenda_widget_3x1=0x7f040001;
+        public static final int agenda_widget_3x2=0x7f040002;
+        public static final int agenda_widget_3x3=0x7f040003;
+        public static final int agenda_widget_4x1=0x7f040004;
+        public static final int agenda_widget_4x2=0x7f040005;
+        public static final int agenda_widget_4x3=0x7f040006;
+        public static final int agenda_widget_4x4=0x7f040007;
     }
 }
